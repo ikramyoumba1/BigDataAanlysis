33eedcf76775715bfd0e2c8bdf2f538e9af6db1a
ikram youmbai <140798019+ikramyoumba1@users.noreply.github.com>
2025-11-26 08:35:52 +0100

    Add files via upload

---
+++ TP7/Consumer.py
@@ -0,0 +1,10 @@
+from kafka import KafkaConsumer
+import json
+
+consumer = KafkaConsumer('machines',
+                         bootstrap_servers='localhost:9092',
+                         auto_offset_reset='earliest',
+                         value_deserializer=lambda v: json.loads(v.decode('utf-8')))
+
+for message in consumer:
+    print(f"Received: {message.value}")
